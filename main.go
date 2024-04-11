11fa3ca963957763d734121f5fcb95fc974730b9
horitomo <ths75149@gmail.com>
2024-04-11 21:58:03 +0900

    テストしやすいように処理をrun関数に分離してテスト作成

--- main.go
+++ main.go
@@ -1,18 +1,62 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-	}))
+func run(ctx context.Context, l net.Listener) error {
 
+	s := &http.Server{
+		// net.Listener でポートは指定される
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
+		}),
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// 別ゴルーチンでHTTPサーバーを起動する
+	eg.Go(func() error {
+		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示す
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの通知（終了通知）を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// 別ゴルーチンの終了の待機
+	return eg.Wait()
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		log.Printf("need port number \n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
 	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
+	// テストしやすいようにrun関数を呼ぶようにする
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
