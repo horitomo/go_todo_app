6e2758678c92891d9fb60b5b7ce94cba6580af9d
horitomo <ths75149@gmail.com>
2024-04-13 14:39:50 +0900

    run の処理を修正

--- main.go
+++ main.go
@@ -5,18 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/horitomo/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 
 	cfg, err := config.New()
 	if err != nil {
@@ -30,33 +24,10 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("Start with: %v", url)
 
-	s := &http.Server{
-		// net.Listener でポートは指定される
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	}
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	eg, ctx := errgroup.WithContext(ctx)
-
-	// 別ゴルーチンでHTTPサーバーを起動する
-	eg.Go(func() error {
-		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示す
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// 別ゴルーチンの終了の待機
-	return eg.Wait()
+	return s.Run(ctx)
 }
 
 func main() {
